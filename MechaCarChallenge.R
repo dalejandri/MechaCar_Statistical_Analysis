860f3146862f141ebaf0c1d764f0e72213d1562c
dalejandri <86029441+dalejandri@users.noreply.github.com>
2021-10-03 13:41:18 -0500

    Deliverable

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,7 +14,7 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table))
 
 
-# Deliverable 2
+# Deliverable 2: Create Visualizations for the Trip Analysis
 
 # In your MechaCarChallenge.R Script, import and read in the Suspension_Coil.csv file as a table.
 
@@ -29,3 +29,21 @@ total_summary = Suspension_Coil_table %>% summarize(Mean=mean(PSI), Median=media
 # to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI column.
 
 lot_summary = Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# Deliverable 3: T-Tests on Suspension Coils
+
+# write three more RScripts in your MechaCarChallenge.R Script using the t.test() 
+# function and its subset() argument to determine if the PSI for each manufacturing lot is statistically 
+# different from the population mean of 1,500 pounds per square inch.
+
+# All lots t-test
+t.test(Suspension_Coil_table$PSI, mu=1500)
+
+# Lot 1 t-test
+t.test(subset(Suspension_Coil_table$PSI, Suspension_Coil_table$Manufacturing_Lot == 'Lot1'), mu=1500)
+
+# Lot 2 t-test
+t.test(subset(Suspension_Coil_table$PSI, Suspension_Coil_table$Manufacturing_Lot == 'Lot2'), mu=1500)
+
+# Lot 3 t-test
+t.test(subset(Suspension_Coil_table$PSI, Suspension_Coil_table$Manufacturing_Lot == 'Lot3'), mu=1500)
